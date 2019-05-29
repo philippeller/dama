e2e885d530a99960780b40f957a70be437a6f955
Philipp Eller <peller@phys.ethz.ch>
2019-05-29 15:00:39 -0400

    table formatting with maximum cols/rows

--- pynocular/utils/formatter.py
+++ pynocular/utils/formatter.py
@@ -20,8 +20,10 @@ See the License for the specific language governing permissions and
 limitations under the License.'''
 
 
-N_MAX = 6
+N_MAX = 12
+'''max rows to display in html formatting'''
 PRECISION = 2
+'''signfificant figures to dispay'''
 
 def as_str(a):
     '''simple string formatter
@@ -41,109 +43,129 @@ def as_str(a):
     return np.array2string(np.asanyarray(a), precision=PRECISION, threshold=2, edgeitems=2)
 
 
-def table_labels(grid, dim):
+def make_table_labels(axis):
     '''generate gird labels
 
     Parameters
     ----------
-    grid : pn.Grid
-    dim : str
+    axis : pn.Axis
 
     Returns
     -------
     labels : list of str
     '''
-    if grid[grid.vars[dim]]._edges is not None:
-        #return ['<b>%s</b>'%(as_str(grid[grid.vars[dim]].edges[i]), as_str(grid[grid.vars[dim]].edges[i+1])) for i in range(grid.shape[dim])]
-        return ['<b>%s</b>'%as_str(grid[grid.vars[dim]].edges[i]) for i in range(grid.shape[dim])]
+    if axis._edges is not None:
+        if len(axis) <= N_MAX:
+            return ['<b>%s</b>'%as_str(axis.edges[i]) for i in range(len(axis))]
+        else:
+            labels = ['<b>%s</b>'%as_str(axis.edges[i]) for i in range(N_MAX//2)]
+            labels += ['...']
+            labels += ['<b>%s</b>'%as_str(axis.edges[i]) for i in range(len(axis)-N_MAX//2, len(axis))]
+            return labels
+    # ToDo: a bit too much copy-paste going on here...
     else:
-        return ['<b>%s</b>'%as_str(grid[grid.vars[dim]].points[i]) for i in range(grid.shape[dim])]
+        if len(axis) <= N_MAX:
+            return ['<b>%s</b>'%as_str(axis.points[i]) for i in range(len(axis))]
+        else:
+            labels = ['<b>%s</b>'%as_str(axis.points[i]) for i in range(N_MAX//2)]
+            labels += ['...']
+            labels += ['<b>%s</b>'%as_str(axis.points[i]) for i in range(len(axis)-N_MAX//2, len(axis))]
+            return labels
+
+
+
+def make_table_row(data):
+    '''forma a simgle table row'''
+    row = ['<b>%s</b>'%data.name]
+    array = np.array(data)
+    if array.shape[0] <= N_MAX:
+        row += [as_str(v) for v in array]
+    else:
+        row += [as_str(v) for v in array[:N_MAX//2]]
+        row += ['...']
+        row += [as_str(v) for v in array[-N_MAX//2:]]
+
+    return row
+
+def make_2d_table(data):
+    x_labels = make_table_labels(data.grid.axes[0])
+    y_labels = make_table_labels(data.grid.axes[1])
+
+    table = []
+    table.append(['<b>%s \\ %s</b>'%(data.grid.vars[1], data.grid.vars[0])] + x_labels)
+    for i in range(len(y_labels)):
+        table.append([y_labels[i]] + [0] * (len(x_labels)))
+
+    n_data_cols, n_data_rows = data.shape[:2]
+
+    if n_data_rows > N_MAX:
+        for i in range(1, min(n_data_cols, 2*(N_MAX//2) + 1) + 1):
+            table[N_MAX//2+1][i] = '...'
+        data_row_idices = list(range(N_MAX//2)) + list(range(n_data_rows - N_MAX//2, n_data_rows))
+        table_row_idices = list(range(N_MAX//2)) + list(range(N_MAX//2+1, 2*(N_MAX//2) + 1))
+    else:
+        data_row_idices = list(range(n_data_rows))
+        table_row_idices = list(range(n_data_rows))
+
+    if n_data_cols > N_MAX:
+        for i in range(1, min(n_data_rows, 2*(N_MAX//2) + 1) + 1):
+            table[i][N_MAX//2+1] = '...'
+        data_col_idices = list(range(N_MAX//2)) + list(range(n_data_cols - N_MAX//2, n_data_cols))
+        table_col_idices = list(range(N_MAX//2)) + list(range(N_MAX//2+1, 2*(N_MAX//2) + 1))
+    else:
+        data_col_idices = list(range(n_data_cols))
+        table_col_idices = list(range(n_data_cols))
+
+    for i, r_idx in zip(table_row_idices, data_row_idices):
+        for j, c_idx in zip(table_col_idices, data_col_idices):
+            table[i+1][j+1] = get_item(data, (c_idx, r_idx))
+            
+    return table
+
+
+def get_item(data, idx):
+    '''Get a string formatted item from a GridArray or gridData object at index idx'''
+    if isinstance(data, pn.GridArray):
+        return as_str(data.data[idx])
+    elif isinstance(data, pn.GridData):
+        # collect all items
+        all_data = []
+        for d in data:
+            all_data.append('%s = %s'%(d.name, as_str(d.data[idx])))
+        return '<br>'.join(all_data)
 
 
 def format_html(data):
     if isinstance(data, pn.PointData):
-        table = [['<b>%s</b>'%a.name] + [as_str(d) for d in np.array(a)] for a in data]
+        table = [make_table_row(a) for a in data]
         return tabulate.tabulate(table, tablefmt='html')
 
     if isinstance(data, pn.PointDataDim):
-        table = [['<b>%s</b>'%data.name] + [as_str(v) for v in np.array(data)]]
+        table = [make_table_row(data)]
         return tabulate.tabulate(table, tablefmt='html')
 
     if isinstance(data, pn.GridArray):
         if data.naxes == 2:
-            table_x = [0] * (data.shape[0] + 1)
-            table = [copy.copy(table_x) for _ in range(data.shape[1] + 1)]
-            
-            table[0][0] = '<b>%s \\ %s</b>'%(data.grid.vars[1], data.grid.vars[0])
-            
-            x_labels = table_labels(data.grid, 0)
-            y_labels = table_labels(data.grid, 1)
-                        
-            for i in range(data.shape[0]):
-                table[0][i+1] = x_labels[i]
-            for i in range(data.shape[1]):
-                table[i+1][0] = y_labels[i]
-                
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    table[j+1][i+1] = as_str(data.data[i, j])
-                    
-            return tabulate.tabulate(table, tablefmt='html')
+            table = make_2d_table(data)
         
         elif data.naxes == 1:
-            table_x = [0] * (data.shape[0] + 1)
-            table = [copy.copy(table_x) for _ in range(2)]
-            table[0][0] = '<b>%s</b>'%data.grid.vars[0]
-            table[1][0] = '<b>%s</b>'%data.name
-            
-            x_labels = table_labels(data.grid, 0)
+            table = []
+            table.append(['<b>%s</b>'%data.grid.vars[0]] + make_table_labels(data.grid.axes[0]))
+            table.append(make_table_row(data))
 
-            
-            for i in range(data.shape[0]):
-                table[0][i+1] = x_labels[i]
-                table[1][i+1] = as_str(data.data[i])
+        return tabulate.tabulate(table, tablefmt='html')
 
-            return tabulate.tabulate(table, tablefmt='html')
-        
+
     if isinstance(data, pn.GridData):
         if data.grid.naxes == 2:
-            table_x = [0] * (data.grid.shape[0] + 1)
-            table = [copy.copy(table_x) for _ in range(data.grid.shape[1] + 1)]
-            
-            table[0][0] = '<b>%s \\ %s</b>'%(data.grid.vars[1], data.grid.vars[0])
-            
-            x_labels = table_labels(data.grid, 0)
-            y_labels = table_labels(data.grid, 1)
-                        
-            for i in range(data.shape[0]):
-                table[0][i+1] = x_labels[i]
-            for i in range(data.shape[1]):
-                table[i+1][0] = y_labels[i]
-                
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    all_data = []
-                    #for var in data.data_vars:
-                    for d in data:
-                        all_data.append('%s = %s'%(d.name, as_str(d.data[i, j])))
-                    table[j+1][i+1] = '<br>'.join(all_data)
-                    
-            return tabulate.tabulate(table, tablefmt='html')
+            table = make_2d_table(data)
         
         elif data.ndim == 1:
-            table_x = [0] * (data.grid.shape[0] + 1)
-            table = [copy.copy(table_x) for _ in range(len(data.data_vars)+1)]
-            table[0][0] = '<b>%s</b>'%data.grid.vars[0]
-            for i, var in enumerate(data.data_vars):
-                table[i+1][0] = '<b>%s</b>'%var
-            
-            x_labels = table_labels(data.grid, 0)
-            
-            for i in range(data.shape[0]):
-                table[0][i+1] = x_labels[i]
-                for j, d in enumerate(data):
-                    table[j+1][i+1] = as_str(d.data[i])
+            table = []
+            table.append(['<b>%s</b>'%data.grid.vars[0]] + make_table_labels(data.grid.axes[0]))
+            for d in data:
+                table.append(make_table_row(d))
 
-            return tabulate.tabulate(table, tablefmt='html')
-        
+        return tabulate.tabulate(table, tablefmt='html')
+
     return data.__repr__()
